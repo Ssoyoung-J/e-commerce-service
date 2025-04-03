c2f2f2f782525fe0665278736d202f58d39ed41f
soy5360 <soy5360@gmail.com>
2025-04-04 05:15:00 +0900

    Feat: swagger 설정

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -1,7 +1,17 @@
 package kr.hhplus.be.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+		info = @Info(title = "E-Commerce Service", version = "1.0", description = "E-Commerce Service API 명세"),
+		servers = {
+				@Server(url = "http://localhost:8080", description = "로컬"),
+		}
+)
 
 @SpringBootApplication
 public class ServerApplication {
