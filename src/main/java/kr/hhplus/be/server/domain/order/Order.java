e55db7ccd8b2c2e1811cb0f7dd9d92736865b997
soy5360 <soy5360@gmail.com>
2025-04-10 03:10:44 +0900

    Feat: 주문 기능 구현 중

---
+++ src/main/java/kr/hhplus/be/server/domain/order/Order.java
@@ -0,0 +1,77 @@
+package kr.hhplus.be.server.domain.order;
+
+import jakarta.persistence.*;
+import kr.hhplus.be.server.domain.common.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Order extends BaseEntity {
+
+    /**
+     *  주문 고유 ID
+     * */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "orderId", nullable = false)
+    private Long orderId;
+
+    /**
+     * 사용자 고유 ID
+     * */
+    @Column(name = "userId", nullable = false)
+    private Long userId;
+
+    /**
+     * 주문 상태
+     * */
+    @Column(name = "orderStatus", nullable = false)
+    private String orderStatus;
+
+    /**
+     * 주문 일시
+     * */
+    @Column(name = "orderedAt", nullable = false)
+    private LocalDateTime orderedAt;
+
+    /**
+     * 상품 총 금액
+     * */
+    @Column(name = "totalAmount", nullable = false)
+    private Long totalAmount;
+
+    /**
+     * 할인 금액
+     * */
+    @Column(name = "discountAmount", nullable = false)
+    private Long discountAmount;
+
+    /**
+     * 최종 결제 금액
+     * */
+    @Column(name = "finalPrice", nullable = false)
+    private Long finalPrice;
+
+    @Builder
+    public Order(Long orderId, Long userId, String orderStatus, LocalDateTime orderedAt, Long totalAmount, Long discountAmount, Long finalPrice) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.orderStatus = orderStatus;
+        this.orderedAt = orderedAt;
+        this.totalAmount = totalAmount;
+        this.discountAmount = discountAmount;
+        this.finalPrice = finalPrice;
+    }
+
+    // 주문 성공 -> orderedStatus = 주문 성공
+//    public String order(Long userId,)
+
+    // 주문 실패 -> orderedStatus = 주문 실패
+
+
+}
