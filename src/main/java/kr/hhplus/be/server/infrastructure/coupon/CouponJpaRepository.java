b56fd6b9f61b0d930843d41e956d496b90dbb5f4
soy5360 <soy5360@gmail.com>
2025-04-17 05:56:39 +0900

    Fix: Coupon 비즈니스 로직 수정

--- src/main/java/kr/hhplus/be/server/infrastructure/coupon/CouponJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/coupon/CouponJpaRepository.java
@@ -1,13 +1,10 @@
-package kr.hhplus.be.server.domain.coupon;
+package kr.hhplus.be.server.infrastructure.coupon;
 
+import kr.hhplus.be.server.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
 
-//    Coupon coupon findByCouponId(Long couponId);
-
-
-    Optional<Coupon> findById(Long couponId);
 }
