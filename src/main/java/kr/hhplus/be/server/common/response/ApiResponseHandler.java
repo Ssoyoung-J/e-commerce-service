57ea6a2d9c94711a583b5b4104505f7b05fe6def
soy5360 <soy5360@gmail.com>
2025-05-06 18:21:51 +0900

    chore: CommonResponse, Exception 관련 설정 추가

---
+++ src/main/java/kr/hhplus/be/server/common/response/ApiResponseHandler.java
@@ -0,0 +1,30 @@
+package kr.hhplus.be.server.common.response;
+
+import kr.hhplus.be.server.presentation.common.CommonResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ApiResponseHandler implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+//        return !CommonResponse.class.isAssignableFrom(returnType.getParameterType());
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+       if(body instanceof CommonResponse) {
+           return body;
+       }
+
+        return CommonResponse.success(body);
+    }
+}
