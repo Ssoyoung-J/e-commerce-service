c12b2b9ceb1e00ce95c4e8f48d1d37e1089cf98f
Ssoyoung-J <soy5360@gmail.com>
2025-05-20 01:22:48 +0900

    chore: redis, mysql test container 설정

--- src/test/java/kr/hhplus/be/server/RedisTestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/RedisTestcontainersConfiguration.java
@@ -9,10 +9,13 @@ import org.testcontainers.utility.DockerImageName;
 public class RedisTestcontainersConfiguration {
 
     public static final RedisContainer REDIS_CONTAINER;
-
     static {
-        REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis:latest"));
+        REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis:7.4.3"))
+                .withExposedPorts(6379);
+
         REDIS_CONTAINER.start();
+        System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+        System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
     }
 
     @PreDestroy
